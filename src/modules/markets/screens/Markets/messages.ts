8ce29eec8ed893402aa45573e07b58ae88b17f39
tomtomcrypto <titiomm@gmail.com>
2022-04-12 10:33:41 +0000

    more language fixs

--- src/modules/markets/screens/Markets/messages.ts
+++ src/modules/markets/screens/Markets/messages.ts
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   native: 'Native',
   pageTitle: 'Markets',
-  pageSubtitle: 'All tokens available to deposit and borrow on Omnidex Lending',
+  pageSubtitle: 'All tokens available to deposit and borrow on OmniLend',
   selectMarket: 'Select a market',
   rewardsInformation: 'Rewards information',
 });
